501a39d709448c93b4f12d20909ccd732568289c
Hasan KARACA <61634253+LapinBIanc@users.noreply.github.com>
2020-06-25 21:36:49 +0300

    Add files via upload

---
+++ sw/soc_prj_43/Debug/src/main.d
@@ -0,0 +1,45 @@
+src/main.o src/main.o: ../src/main.cc \
+ ../../soc_prj_43_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../soc_prj_43_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../soc_prj_43_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../soc_prj_43_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../src/rgb_image.h ../../soc_prj_43_bsp/ps7_cortexa9_0/include/xtime_l.h \
+ ../../soc_prj_43_bsp/ps7_cortexa9_0/include/ximageproc.h \
+ ../../soc_prj_43_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../soc_prj_43_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../soc_prj_43_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../soc_prj_43_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../soc_prj_43_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../soc_prj_43_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../soc_prj_43_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../soc_prj_43_bsp/ps7_cortexa9_0/include/ximageproc_hw.h
+
+../../soc_prj_43_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../soc_prj_43_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../soc_prj_43_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../soc_prj_43_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../src/rgb_image.h:
+
+../../soc_prj_43_bsp/ps7_cortexa9_0/include/xtime_l.h:
+
+../../soc_prj_43_bsp/ps7_cortexa9_0/include/ximageproc.h:
+
+../../soc_prj_43_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../soc_prj_43_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../soc_prj_43_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../soc_prj_43_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../soc_prj_43_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../soc_prj_43_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../soc_prj_43_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../soc_prj_43_bsp/ps7_cortexa9_0/include/ximageproc_hw.h:
